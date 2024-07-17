c4ea4b6d1b017838abf55db4b80b602e4c678342
Roxeraf <60817078+Roxeraf@users.noreply.github.com>
2024-07-17 11:15:41 +0000

    Beschreibe deine Änderungen

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,22 @@
 import streamlit as st
 import pandas as pd
 import json
+from io import BytesIO
 
 def parse_json_columns(df, json_columns):
-    # Funktion zum sicheren Parsen von JSON-Strings in mehreren Spalten
     def try_loads(x):
         try:
             return json.loads(x)
         except (TypeError, json.JSONDecodeError):
             return None
 
-    parsed_dfs = []
-    
     for json_column in json_columns:
         parsed_data = df[json_column].apply(try_loads)
-        json_df = pd.json_normalize(parsed_data.dropna())
-        json_df.columns = [f"{json_column}.{col}" for col in json_df.columns]  # Prefix the columns with the original column name
-        parsed_dfs.append(json_df)
-
-    df = df.drop(columns=json_columns)  # Remove the original JSON columns
-    parsed_df = pd.concat([df] + parsed_dfs, axis=1)  # Concatenate the original dataframe with all parsed JSON dataframes
+        json_df = pd.json_normalize(parsed_data)
+        json_df.columns = [f"{json_column}.{col}" for col in json_df.columns]
+        df = df.drop(columns=[json_column]).join(json_df, how='left')
     
-    return parsed_df
+    return df
 
 st.title("File to JSON Parser")
 
@@ -54,9 +49,12 @@ if uploaded_file:
             st.download_button(label="Download as CSV", data=csv, file_name='parsed_data.csv', mime='text/csv')
 
             # Option zum Herunterladen der aufbereiteten Daten als Excel-Datei
-            excel_buffer = pd.ExcelWriter("parsed_data.xlsx", engine='xlsxwriter')
-            parsed_df.to_excel(excel_buffer, index=False)
-            excel_buffer.save()
-            st.download_button(label="Download as Excel", data=excel_buffer, file_name='parsed_data.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+            output = BytesIO()
+            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                parsed_df.to_excel(writer, index=False, sheet_name='Sheet1')
+                writer.save()
+            output.seek(0)
+            st.download_button(label="Download as Excel", data=output, file_name='parsed_data.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
         except Exception as e:
             st.error(f"An error occurred: {e}")
+
