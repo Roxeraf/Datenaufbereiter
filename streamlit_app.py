649056079897dfe4d8de0a53095f1821785f59f4
Roxeraf <60817078+Roxeraf@users.noreply.github.com>
2024-07-17 08:37:54 +0000

    Update --readability CSV and txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,24 @@ import pandas as pd
 import json
 
 def parse_json_column(df, json_column):
-    # Diese Funktion wandelt JSON-Strings in einer Spalte in ein DataFrame um
     parsed_data = df[json_column].apply(json.loads)
     json_df = pd.json_normalize(parsed_data)
     return pd.concat([df.drop(columns=[json_column]), json_df], axis=1)
 
-st.title("Excel to JSON Parser")
+st.title("File to JSON Parser")
 
-uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls"])
+uploaded_file = st.file_uploader("Upload a file", type=["xlsx", "xls", "csv", "txt"])
 
 if uploaded_file:
-    sheet_names = pd.ExcelFile(uploaded_file).sheet_names
-    sheet_name = st.selectbox("Select sheet", sheet_names)
-
-    df = pd.read_excel(uploaded_file, sheet_name=sheet_name)
+    file_type = uploaded_file.name.split('.')[-1]
+    if file_type in ['xlsx', 'xls']:
+        sheet_names = pd.ExcelFile(uploaded_file).sheet_names
+        sheet_name = st.selectbox("Select sheet", sheet_names)
+        df = pd.read_excel(uploaded_file, sheet_name=sheet_name)
+    elif file_type == 'csv':
+        df = pd.read_csv(uploaded_file)
+    elif file_type == 'txt':
+        df = pd.read_csv(uploaded_file, delimiter='\t')
 
     st.write("Original Data:")
     st.dataframe(df)
